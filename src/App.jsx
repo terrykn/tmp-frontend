5aba84bbd4b9e0b185016ee38da637f7aeb465af
Terry Nguyen <terryknguyen@gmail.com>
2023-10-07 17:56:21 -0400

    HACKRU commit 1, frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,67 @@
+import { Avatar } from '@mui/material';
 import './App.css';
-import GeneratePlaylist from './components/GeneratePlaylist';
+
 import TravelInput from './components/TravelInput';
+import TravelOutput from './components/TravelOutput';
+
+import { useState } from 'react';
 
 
 function App() {
+  const [travelData, setTravelData] = useState({
+    start: "N/A",
+    end: "N/A",
+    travelTime: 0,
+  })
+
+  const generateRoute = (from, destination) => {
+    const travelTime = calculateTravelTime(from, destination);
+
+    setTravelData({
+      start: from,
+      end: destination,
+      travelTime: travelTime,
+    });
+  };
+
+  const calculateTravelTime = (from, destination) => {
+    return Math.floor(Math.random() * 60) + 1; // random travel time for now
+  }
+
   return (
-    <>
+    <div sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
       {/* Header */}
       <div className="Header">
-        <h1>Travel Music Planner</h1>
+        
+        <h1>Trip Music Playlist</h1>
       </div>
 
-      {/* Traveling */}
+      {/* Generate Route from TravelInput */}
       <div className="App">
-        <TravelInput />
+        <TravelInput onGenerateRoute={generateRoute} />
       </div>
 
-      {/* Generate Spotify Playlist */}
+      {/* Generate Playlist from TravelOutput */}
       <div className="App">
-        <GeneratePlaylist />
+        <TravelOutput
+          start={travelData.start}
+          end={travelData.end}
+          travelTime={travelData.travelTime} 
+        />
       </div>
 
       {/* Footer */}
       <div className="Footer">
-        
+        <Avatar 
+          src="/assets/njtransit2-logo.jpg" 
+          sx={{ width: 80, height: 80 }}
+        />
+        <Avatar 
+          src="/assets/spotify-logo.png"
+          sx={{ width: 70, height: 70 }} 
+        />
       </div>
-    </>
+    </div>
   );
 }
 
